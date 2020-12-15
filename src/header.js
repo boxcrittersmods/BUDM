68e40c5a9a38f73535a397a32f3ef7f260329e0c
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-12-15 15:55:35 -0500

    switch to regex @include

--- src/header.js
+++ src/header.js
@@ -2,12 +2,7 @@
 // @name         United Program Interface
 // @author       SArpnt and TumbleGamer
 // @supportURL   http://discord.gg/D2ZpRUW
-// @match        https://boxcritters.com/play/
-// @match        https://boxcritters.com/play/?*
-// @match        https://boxcritters.com/play/#*
-// @match        https://boxcritters.com/play/index.html
-// @match        https://boxcritters.com/play/index.html?*
-// @match        https://boxcritters.com/play/index.html#*
+// @include      /^https:\/\/boxcritters\.com\/play\/(index\.html)?([\?#].*)?$/
 // @run-at       document-start
 // @require      https://github.com/SArpnt/EventHandler/raw/master/script.js
 // ==/UserScript==
